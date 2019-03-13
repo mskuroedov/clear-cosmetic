b8861a41c371f757115863c077bc6b48d1587f3d
kuroedov_maxim <Ni7pfpNh>
2019-03-13 17:29:07 +0300

    mobile menu added

---
+++ js/mobile-menu.js
@@ -0,0 +1,22 @@
+//mobile menu scripts
+
+
+function openMobileMenu(){
+    $('body').addClass('mobile-menu-opened');
+    $('.mobile-menu').css('display','block');
+    setTimeout(function () {
+        $('.mobile-menu').addClass('active');
+    },150);
+}
+
+function closeMobileMenu(){
+    $('body').removeClass('mobile-menu-opened');
+    $('.mobile-menu').removeClass('active');
+    setTimeout(function () {
+        $('.mobile-menu').css('display','none');
+    },150);
+}
+
+$('.burger').on('click',openMobileMenu);
+$('.mobile-menu .close-block a').on('click',closeMobileMenu)
+$('.mobile-menu .bg').on('click',closeMobileMenu)
