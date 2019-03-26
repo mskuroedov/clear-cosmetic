df88b3af37f37a05e254011bbcd11943a2e1fe67
kuroedov_maxim <Ni7pfpNh>
2019-03-26 11:29:30 +0300

    animation blocks

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,17 @@
 window.addEventListener('load', (event) => {
-    $('.loader').animate({
+    $('.loader img').animate({
         opacity:0
     },500);
 
     setTimeout(function () {
+        $('.loader').animate({
+            opacity:0
+        },200);
+    },500);
+    setTimeout(function () {
         $('header').addClass('fadeFromTop');
-    },200)
+
+    },500);
     setTimeout(function () {
         $('.loader').hide();
     },600);
